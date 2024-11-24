393e214e298a0b151e224d1571dc33309b65f2ae
sanjana2975 <112097355+sanjana2975@users.noreply.github.com>
2024-11-24 18:42:16 +0530

    Revert "Add files via upload"
    This reverts commit 37875c97d965cd7a4406f650157c131eac5d84f3.

--- src/data/UserData.js
+++
@@ -1,7 +0,0 @@
-const user = {
-  id: 1,
-  name: "John Doe",
-  enrolledBookClubs: ["JavaScript Mastery", "Quality Engineering Book Club"]
-};
-
-export default user;
--- src/store/currentUserStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand'
-
-
-export const currentUserStore = create((set) => ({
-    userName: '',
-    // The setUserName function should accept the new value as a parameter
-    setUserName: (newName) => set({ userName: newName })
-}));
